18c45858b9a2ae68e21d8498dd275dfe42a3ac74
dhiyo7 <dhiyoreksa12@gmail.com>
2019-11-21 21:28:21 +0700

    change strukturuk

---
+++ app/src/main/java/com/plugin/pregnantcarekuwuk/FragmentUser/MapsFragment.java
@@ -0,0 +1,32 @@
+package com.plugin.pregnantcarekuwuk.FragmentUser;
+
+
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.plugin.pregnantcarekuwuk.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ *
+ */
+public class MapsFragment extends Fragment {
+
+
+    public MapsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_maps, container, false);
+    }
+
+}
