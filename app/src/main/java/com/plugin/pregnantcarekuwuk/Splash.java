abe28a92c341b08205a218f042103a58f8ee82b1
AkhmadTantowi <tantowix72@gmail.com>
2019-11-21 19:16:11 +0700

    splash login register tapi durung intent

--- app/src/main/java/com/plugin/pregnantcarekuwuk/Splash.java
+++ app/src/main/java/com/plugin/pregnantcarekuwuk/Splash.java
@@ -2,13 +2,34 @@ package com.plugin.pregnantcarekuwuk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.widget.ImageView;
+
 
 public class Splash extends AppCompatActivity {
+
+    ImageView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash);
-    }
+
+        logo = (ImageView) findViewById(R.id.logo);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                finish();
+            }
+        }, 3000L);
+
+     }
+
 }
