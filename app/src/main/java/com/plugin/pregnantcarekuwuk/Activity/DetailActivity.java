486e5d35db81370cb2cc53dcdfbe4766d3052d97
AkhmadTantowi <tantowix72@gmail.com>
2019-11-23 00:20:28 +0700

    hoke

--- app/src/main/java/com/plugin/pregnantcarekuwuk/Activity/DetailActivity.java
+++ app/src/main/java/com/plugin/pregnantcarekuwuk/Activity/DetailActivity.java
@@ -3,14 +3,27 @@ package com.plugin.pregnantcarekuwuk.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.plugin.pregnantcarekuwuk.R;
 
 public class DetailActivity extends AppCompatActivity {
 
+    TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        getSupportActionBar().setTitle("DetailActivity");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return super.onSupportNavigateUp();
     }
 }
--- app/src/main/java/com/plugin/pregnantcarekuwuk/Splash.java
+++ app/src/main/java/com/plugin/pregnantcarekuwuk/Splash.java
@@ -33,7 +33,7 @@ public class Splash extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
                 finish();
             }
         }, 3000L);
