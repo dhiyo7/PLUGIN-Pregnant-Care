7260524f360f722069521f78c66511415c9fca9b
dhiyo7 <dhiyoreksa12@gmail.com>
2019-11-24 14:11:58 +0700

    Maps & Profile Done (User)

--- app/src/main/java/com/plugin/pregnantcarekuwuk/Activity/LoginActivity.java
+++ app/src/main/java/com/plugin/pregnantcarekuwuk/Activity/LoginActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 import com.plugin.pregnantcarekuwuk.R;
@@ -17,6 +18,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
 
         initView();
         intent();
